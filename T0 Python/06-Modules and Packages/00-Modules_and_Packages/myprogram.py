cad041cb3ad2cac99ecaa98f8d2f4ed25d9e69eb
Alberto Saz <albertosaz@gmail.com>
2023-10-03 11:10:36 +0200

    mas teoria

--- T0 Python/06-Modules and Packages/00-Modules_and_Packages/myprogram.py
+++ T0 Python/06-Modules and Packages/00-Modules_and_Packages/myprogram.py
@@ -1,6 +1,6 @@
-from MyMainPackage.some_main_script import report_main
-from MyMainPackage.SubPackage import mysubscript
+from MyMainPackage.some_main_script import report_main # Importing a function from a package
+from MyMainPackage.SubPackage import mysubscript # Importing all from a subpackage
 
-report_main()
+report_main() # Calling a function from a package
 
-mysubscript.sub_report()
+mysubscript.sub_report() # Calling a function from a subpackage
